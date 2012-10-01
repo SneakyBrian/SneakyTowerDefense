7c71be3616a2af33ee7b3c604b7f400568d94aa7
SneakyBrian <SneakyBrian@gmail.com>
2012-10-01 20:57:20 +0100

    Added simple painting demo to Game Controller Index Action  View, just to check that EaselJS was working ok.

---
+++ SneakyTowerDefense.Mvc.Web/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SneakyTowerDefense.Mvc.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
